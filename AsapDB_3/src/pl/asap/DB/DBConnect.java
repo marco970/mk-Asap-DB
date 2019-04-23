f7c2a2b33c7d13b42ba505a117b4fd27b3646867
marco970 <m.kuciak@gmail.com>
2019-04-23 15:49:24 +0200

    notoatka wizualnie ok, teraz trzeba logicznie ogarnąć

--- AsapDB_3/src/pl/asap/DB/DBConnect.java
+++ AsapDB_3/src/pl/asap/DB/DBConnect.java
@@ -3,10 +3,8 @@ package pl.asap.DB;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
