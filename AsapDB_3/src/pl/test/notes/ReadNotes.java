1f68fbb9cb4bfef9ba900ba5df5a84a7dd29a076
marco970 <m.kuciak@gmail.com>
2019-05-06 15:29:58 +0200

    dodanie ReadNotes do NotesScreen

--- AsapDB_3/src/pl/test/notes/ReadNotes.java
+++ AsapDB_3/src/pl/test/notes/ReadNotes.java
@@ -1,5 +1,8 @@
 package pl.test.notes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Query;
 
 import org.hibernate.Session;
@@ -12,19 +15,14 @@ import pl.asap.entity.Notes;
 public class ReadNotes {
 	
 	private static int idPostepowanie;
+	private ArrayList<Notes> notes;
+	//private static Lista lista = new Lista();
 	
 	
 
 	public ReadNotes(int idPostepowanie) {
 		this.idPostepowanie = idPostepowanie;
-	}
-
-
-
-	public static void main(String[] args) {
-		
-		new ReadNotes(11);
-		
+		notes = new ArrayList<Notes>();
 		// stworzenie obiektu Configuration
 		Configuration conf = new Configuration();
 		// wczytanie pliku konfiguracyjnego
@@ -37,28 +35,32 @@ public class ReadNotes {
 		SessionFactory factory = conf.buildSessionFactory();
 		// pobranie sesji
 		Session session = factory.getCurrentSession();
-
-		
 		String getPostepowanie = "select c from Lista c where c.idPostepowanie="+idPostepowanie;
-
-		session.beginTransaction();
-		
+		session.beginTransaction();		
 		Query query = session.createQuery(getPostepowanie);
-		
-		Lista lista = (Lista) query.getSingleResult();
-		
+		Lista lista = (Lista) query.getSingleResult();		
 		System.out.println(lista.toString());
 		
 		for(Notes note: lista.getNotes() )	{
+			
+			notes.add(note);
 			System.out.println(note.toString());
 		}
 		
 		session.getTransaction().commit();
-
-
-
-		// zamkniêcie obiektu SessionFactory
 		factory.close();
+	}
+	public ArrayList<Notes> getNotes()	{
+		return notes;
+	}
+
+
+
+	public static void main(String[] args) {
+		
+		new ReadNotes(12);
+		
+
 
 	}
 
