031f5d08dfe909a562ea8117f8a84df458fa193b
marco970 <m.kuciak@gmail.com>
2019-06-27 15:23:18 +0200

    początek prac nad delete

---
+++ AsapDB_3/src/pl/asap/transactions/NoteDelete.java
@@ -0,0 +1,18 @@
+package pl.asap.transactions;
+
+import pl.asap.entity.EntityBase;
+
+public class NoteDelete extends TransBlank {
+	
+
+	public NoteDelete(EntityBase bean) {
+		super(bean);
+
+	}
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
