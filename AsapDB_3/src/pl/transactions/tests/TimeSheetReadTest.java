d9a781549bd2454a154e62eeab4880a860dbcc88
Marcin <m.kuciak@gmail.com>
2019-08-20 23:27:59 +0200

    nnn

--- AsapDB_3/src/pl/transactions/tests/TimeSheetReadTest.java
+++ AsapDB_3/src/pl/transactions/tests/TimeSheetReadTest.java
@@ -1,6 +1,7 @@
 package pl.transactions.tests;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Query;
 
@@ -35,13 +36,17 @@ public class TimeSheetReadTest {
 		Session session = factory.getCurrentSession();
 		
 		String getPostepowanie = "select c from Lista c where c.idPostepowanie="+idPostepowanie;
+		String hql = "from TimeSheetEntity t join ";
 		session.beginTransaction();		
-		Query query = session.createQuery(getPostepowanie);
+		Query query = session.createQuery(hql);
+//		
+//		Lista lista = (Lista) query.getSingleResult();		
+//		System.out.println(lista.toString());
 		
-		Lista lista = (Lista) query.getSingleResult();		
-		System.out.println(lista.toString());
+//		TimeSheetEntity lista = session.get(TimeSheetEntity.class, 1);
+		List<TimeSheetEntity> lista = query.getResultList();
 		
-		for(TimeSheetEntity entry: lista.getEntries())	{
+		for(TimeSheetEntity entry: lista)	{
 			System.out.println("resultCheck -> "+checkDate(entry, mounth, year)+" --> " +mounth+" vs "+entry.getDateEntry().substring(3, 5));
 			System.out.println(" --> " +year+" vs "+entry.getDateEntry().substring(6, 10));
 			if (checkDate(entry, mounth, year)) {
@@ -50,7 +55,7 @@ public class TimeSheetReadTest {
 			}
 		}
 		
-		System.out.println(entries.toString());
+		System.out.println(lista);
 		
 		session.getTransaction().commit();
 		factory.close();
