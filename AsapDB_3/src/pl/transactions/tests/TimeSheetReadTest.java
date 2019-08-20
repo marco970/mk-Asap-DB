06576f4e27d9b631f5eceae61ae93bddf8c6ede3
Marcin <m.kuciak@gmail.com>
2019-08-20 22:19:55 +0200

    uzupełnienie brakującej funkcjonalności

---
+++ AsapDB_3/src/pl/transactions/tests/TimeSheetReadTest.java
@@ -0,0 +1,80 @@
+package pl.transactions.tests;
+
+import java.util.ArrayList;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import pl.asap.entity.Config;
+import pl.asap.entity.Lista;
+import pl.asap.entity.Notes;
+import pl.asap.entity.TimeSheetEntity;
+import pl.asap.transactions.TransBlank;
+
+public class TimeSheetReadTest {
+	
+	private ArrayList<TimeSheetEntity> entries;
+	private int mounth;
+
+	public TimeSheetReadTest(int idPostepowanie, int mounth, int year) {
+		
+		// TODO Auto-generated constructor stub
+		
+		this.entries= new ArrayList<>();
+		this.mounth = mounth;
+		
+		Configuration conf = new Configuration();
+		Config config = new Config();
+		conf.configure(config.getHibernateXML());
+		config.setAnnotatedClass(conf);
+		
+		SessionFactory factory = conf.buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		
+		String getPostepowanie = "select c from Lista c where c.idPostepowanie="+idPostepowanie;
+		session.beginTransaction();		
+		Query query = session.createQuery(getPostepowanie);
+		
+		Lista lista = (Lista) query.getSingleResult();		
+		System.out.println(lista.toString());
+		
+		for(TimeSheetEntity entry: lista.getEntries())	{
+			System.out.println("resultCheck -> "+checkDate(entry, mounth, year)+" --> " +mounth+" vs "+entry.getDateEntry().substring(3, 5));
+			System.out.println(" --> " +year+" vs "+entry.getDateEntry().substring(6, 10));
+			if (checkDate(entry, mounth, year)) {
+				System.out.println(entry.getDateEntry().substring(3, 5));
+				entries.add(entry);
+			}
+		}
+		
+		System.out.println(entries.toString());
+		
+		session.getTransaction().commit();
+		factory.close();
+		
+	}
+	public boolean checkDate(TimeSheetEntity entry, int month, int year)	{
+//		System.out.println();
+		System.out.println("year-> "+entry.getDateEntry().substring(6, 10));
+		if (entry.getDateEntry().substring(3, 5).equals(monthString(month)) && entry.getDateEntry().substring(6, 10).equals(year+"")) return true;
+		else return false;
+		
+	}
+	public String monthString(int month)	{
+		String monthStr;
+		if (month<10) monthStr = "0"+month;
+		else monthStr = ""+month;
+		System.out.println(monthStr+" <--");
+		return monthStr;
+	}
+	
+	
+	public static void main(String[] args) {
+		new TimeSheetReadTest(35, 8, 2019);
+
+	}
+
+}
