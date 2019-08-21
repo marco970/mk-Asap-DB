75ba8146848dc5a341df099279ae6765d3e9634e
marco970 <m.kuciak@gmail.com>
2019-08-21 15:42:37 +0200

    walka z wyłapaniem aktywnych postępowań....

--- AsapDB_3/src/pl/transactions/tests/TimeSheetReadTest.java
+++ AsapDB_3/src/pl/transactions/tests/TimeSheetReadTest.java
@@ -1,6 +1,7 @@
 package pl.transactions.tests;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.Query;
@@ -18,14 +19,20 @@ import pl.asap.transactions.TransBlank;
 public class TimeSheetReadTest {
 	
 	private ArrayList<TimeSheetEntity> entries;
+	private ArrayList<Integer> idPostepowanieAktywne;
+	private ArrayList<Lista> queryArr;
+	private HashMap mapa;
 	private int mounth;
 
 	public TimeSheetReadTest(int idPostepowanie, int mounth, int year) {
 		
 		// TODO Auto-generated constructor stub
 		
-		this.entries= new ArrayList<>();
+		this.entries = new ArrayList<>();
 		this.mounth = mounth;
+		this.idPostepowanieAktywne = new ArrayList<>();
+		this.queryArr = new ArrayList<>();
+		this.mapa = new HashMap();
 		
 		Configuration conf = new Configuration();
 		Config config = new Config();
@@ -35,30 +42,69 @@ public class TimeSheetReadTest {
 		SessionFactory factory = conf.buildSessionFactory();
 		Session session = factory.getCurrentSession();
 		
-		String getPostepowanie = "select c from Lista c where c.idPostepowanie="+idPostepowanie;
-		String hql = "from TimeSheetEntity t join ";
+		String getActivePostepowanie = "select c from Lista c where c.Status='aktywne'";
+//		String hql = "from TimeSheetEntity t join ";
 		session.beginTransaction();		
-		Query query = session.createQuery(hql);
+		Query query = session.createQuery(getActivePostepowanie);
 //		
-//		Lista lista = (Lista) query.getSingleResult();		
+		List<Lista> lista = query.getResultList();		
+		int i = 0;
+		for (Lista el: lista)	{
+			
+			System.out.println("aaa--> "+el.toString());
+			idPostepowanieAktywne.add(el.getIdPostepowanie());
+			String getEntries = "select c from Lista c where c.idPostepowanie="+el.getIdPostepowanie();
+			Query query2 = session.createQuery(getEntries);
+			Lista lista2 =  (Lista) query2.getSingleResult();
+//			System.out.println("ccc-> "+lista2.getEntries().toString());
+			
+			if (lista2.getEntries().toString().equals("[]")) {
+				
+			} else {
+				queryArr.add(lista2);
+				mapa.put(el.getIdPostepowanie(), lista2);
+			}
+			
+			
+//			
+
+			lista2 = null;
+			query2 = null;
+			i++;
+		}
+		for (Object elem:mapa.keySet())	{
+			System.out.println("bbb-> "+elem+" --> "+((Lista) mapa.get(elem)).getEntries().toString());
+		}
 //		System.out.println(lista.toString());
 		
 //		TimeSheetEntity lista = session.get(TimeSheetEntity.class, 1);
-		List<TimeSheetEntity> lista = query.getResultList();
+//		List<TimeSheetEntity> lista = query.getResultList();
 		
-		for(TimeSheetEntity entry: lista)	{
-			System.out.println("resultCheck -> "+checkDate(entry, mounth, year)+" --> " +mounth+" vs "+entry.getDateEntry().substring(3, 5));
-			System.out.println(" --> " +year+" vs "+entry.getDateEntry().substring(6, 10));
-			if (checkDate(entry, mounth, year)) {
-				System.out.println(entry.getDateEntry().substring(3, 5));
-				entries.add(entry);
-			}
-		}
+//		for(TimeSheetEntity entry: lista)	{
+//			System.out.println("resultCheck -> "+checkDate(entry, mounth, year)+" --> " +mounth+" vs "+entry.getDateEntry().substring(3, 5));
+//			System.out.println(" --> " +year+" vs "+entry.getDateEntry().substring(6, 10));
+//			if (checkDate(entry, mounth, year)) {
+//				System.out.println(entry.getDateEntry().substring(3, 5));
+//				entries.add(entry);
+//			}
+//		}
 		
-		System.out.println(lista);
+//		System.out.println(lista);
 		
 		session.getTransaction().commit();
+		session.close();
+//		
+//		Session session2 = factory.getCurrentSession();
+//		session2.beginTransaction();	
+//		
+//		
+//		
+//		
+//		session2.getTransaction().commit();
+//		session2.close();
 		factory.close();
+		
+		
 		
 	}
 	public boolean checkDate(TimeSheetEntity entry, int month, int year)	{
