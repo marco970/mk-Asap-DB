d1f632526b6130806f0cd9bc85d12aa3a6ced702
Marcin <m.kuciak@gmail.com>
2019-09-18 20:00:57 +0200

    nowa koncepcja w extraktorze

--- AsapDB_3/src/pl/asap/raport/DataExtractor2.java
+++ AsapDB_3/src/pl/asap/raport/DataExtractor2.java
@@ -1,7 +1,7 @@
 package pl.asap.raport;
-
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.PropertyConfigurator;
+//
+//import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.PropertyConfigurator;
 
 import pl.asap.models.TimeSheetModel3;
 
@@ -21,30 +21,6 @@ public class DataExtractor2 {
 			System.out.println();
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-	public static void main(String[] args) {
-
-//		BasicConfigurator.configure();
-		String log4jConfPath = "D:\\GitRepo\\mk-Asap-DB\\AsapDB_3\\log4j.properties";
-		PropertyConfigurator.configure(log4jConfPath);
-		
-		TimeSheetModel3 tsm3 = new TimeSheetModel3(9, 2019);
-		new DataExtractor2(tsm3);
-
-	}
 
 	public Object getExRow(String string) {
 		
@@ -61,5 +37,19 @@ public class DataExtractor2 {
 		
 		return null;
 	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+
+//		BasicConfigurator.configure();
+		String log4jConfPath = "D:\\GitRepo\\mk-Asap-DB\\AsapDB_3\\log4j.properties";
+//		PropertyConfigurator.configure(log4jConfPath);
+		
+		TimeSheetModel3 tsm3 = new TimeSheetModel3(9, 2019);
+		new DataExtractor2(tsm3);
+
+	}
+
 
 }
