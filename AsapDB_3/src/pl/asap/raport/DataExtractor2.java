c12c1d062492394e5841c7e63813f0eb75f69976
marco970 <m.kuciak@gmail.com>
2019-09-18 15:29:47 +0200

    początek działania z xls

---
+++ AsapDB_3/src/pl/asap/raport/DataExtractor2.java
@@ -0,0 +1,54 @@
+package pl.asap.raport;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+import pl.asap.models.TimeSheetModel3;
+
+public class DataExtractor2 {
+	
+	
+	
+	public DataExtractor2(TimeSheetModel3 tsm3)	{
+		
+//		i - wiersze
+//		j - kolumny
+		
+		for (int i = 0; i < tsm3.getRowCount(); i++)		{
+			for (int j = 0; j < tsm3.getColumnCount(); j++)	{
+				System.out.print(tsm3.getValueAt(i, j)+" | ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+
+//		BasicConfigurator.configure();
+		String log4jConfPath = "D:\\GitRepo\\mk-Asap-DB\\AsapDB_3\\log4j.properties";
+		PropertyConfigurator.configure(log4jConfPath);
+		
+		TimeSheetModel3 tsm3 = new TimeSheetModel3(9, 2019);
+		new DataExtractor2(tsm3);
+
+	}
+
+	public Object getExRow(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
